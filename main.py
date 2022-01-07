cb802b6f8c393adad30e165a429e6344b7e7799e
mattBright <mateuszwronski.yt@gmail.com>
2022-01-07 15:55:21 +0100

    little appearance mod

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from typing_extensions import runtime
+from asyncio import events
 import discord, time, logging
+from discord import client
 from discord.ext import commands
+from discord.member import Member
 from discord.user import User
-from discord.app import Option
+from discord import Option
 
 #logging
 logging.basicConfig(level=logging.INFO)
 
 #useful variables
+commandsNum = 9
 runStarted = time.perf_counter()
 bot = discord.Bot()
 UserMatt = bot.get_or_fetch_user(650343691998855188)
@@ -24,7 +27,23 @@ async def on_ready():
     global meUser
     meUser = await bot.get_or_fetch_user(650343691998855188)
     print("The prefix is /")
-    print('Bot is currently: Online')
+    print(f'{bot.user.name} is currently: Online')
+
+@bot.command()
+async def ban(ctx, user: Option(Member, 'Użytkownik do zbanowania'), rsn: Option(str, 'Powód bana')):
+    await user.ban(delete_message_days=0, reason=rsn)
+    await ctx.respond(f'🔨 Użytkownik {user.mention}, zostałx zbanowanx')
+
+@bot.command()
+async def help(ctx):
+    msg = discord.Embed(title='POMOC', color=0x1ABC9C, )
+    msg.remove_author
+    global commandsNum
+    msg.add_field(name="INFO",  value=f'Hej {ctx.author.mention}, jestem tu żeby ci pomóc. Posiadam {str(commandsNum)} komend i ciągle jestem rozwijany', inline=False)
+    msg.add_field(name="Lista komend", value=f'``/help user``\n``/help profile``\n``/help eko``\n``/help music``\n``/help admin``\n``/help owner``', inline=True)
+    msg.add_field(name='FAQ', value='1. Nie szukam collaba\n2. Bota można zakupić lub otrzymać bezpłatnie ode mnie\n3. Nie pisać na /contact jeżeli sprawa nie związana z botem', inline=True)
+    msg.add_field(name='Linki', value='``🖥 vps`` https://mc.polishwrona.pl\n``📜 blog`` https://polishwrona.pl\n``🐈 git`` https://github.com/mattBrighto', inline=True)
+    await ctx.respond(embed=msg)
 
 #ping embeded command
 @bot.command()
@@ -33,7 +52,7 @@ async def ping(ctx):
     msg.remove_author()
     secs = abs(runStarted - time.perf_counter())
     runtiming = time.strftime("%H godzin %M minut %S sekund", time.gmtime(secs))
-    msg.add_field(name='------------', value=f'Jestem online, a mój ping wynosi ``{round(bot.latency * 1000)} ms``\nJestem włączony od : ``{runtiming}``', inline=False)
+    msg.add_field(name='------------', value=f'Jestem online, a mój ping wynosi ``💨{round(bot.latency * 1000)} ms``\n⏰ Jestem włączony od : ``{runtiming}``', inline=False)
     await ctx.respond(embed=msg)
 
 #info embeded command
@@ -44,8 +63,8 @@ async def info(ctx):
     msg.add_field(name='Basic Info', value=f'Hej jestem {meMention}. Zrobiłem tego bota For Fun, ale jeśli chciałbyś, abym wykonał jakiś projekt informatyczny/programistyczny skorzystaj z komendy /contact (oczywiście z odpowiednim prefixem) żeby się ze mną skontaktować')
     secs = abs(runStarted - time.perf_counter())
     runtiming = time.strftime("%H godzin %M minut %S sekund", time.gmtime(secs))
-    msg.add_field(name='Info Bota', value=f'``runtime:``   {runtiming}\n ``ping`` :	{round(bot.latency * 1000)}  ms')
-    msg.add_field(name='Linki', value='``PERSONAL SITE``   https://mc.polishwrona.pl/', inline=False)
+    msg.add_field(name='Info Bota', value=f'``⏰ runtime:``   {runtiming}\n ``💨 ping`` :	{round(bot.latency * 1000)}  ms')
+    msg.add_field(name='Linki', value='``🔗 PERSONAL SITE``   https://mc.polishwrona.pl/', inline=False)
     await ctx.respond(embed=msg)
 
 #contact limited command
@@ -53,27 +72,27 @@ async def info(ctx):
 async def contact(ctx, msg):
     list = []
     if ctx.author.id in list:
-        await ctx.respond("Musisz zaczekać zanim wyślesz kolejną wiadomość", delete_after = 1)
+        await ctx.respond("⌛ Musisz zaczekać zanim wyślesz kolejną wiadomość", delete_after = 1)
     else:
         global meUser
         dm = await meUser.create_dm()
         await dm.send(f'New message from {ctx.author.mention}: '+msg)
         list.append(int(ctx.author.id))
-        await ctx.respond(f"||{ctx.author.mention}|| Twoja wiadomość została wysłana")
+        await ctx.respond(f"||{ctx.author.mention}|| Twoja wiadomość została wysłana ✅")
 
 #say owner command
 @bot.command()
 async def say(ctx, msg):
     if ctx.author.id != 650343691998855188:
-        await ctx.respond("Nie masz wystarczających uprawnień :<", delete_after = 1)
+        await ctx.respond("⛔ Nie masz wystarczających uprawnień :<", delete_after = .5)
     if ctx.author.id == 650343691998855188:
         await ctx.send(msg)
         
-#clear messages command.... but still want to display deleted messages not how many were supposed to be
+#clear messages command
 @bot.command()
-async def clear(ctx, x):
-    await ctx.channel.purge(limit=int(x))
-    await ctx.respond(f'Pomyślnie usunięto {x} wiadomości!', delete_after = 1)
+async def clear(ctx, number):
+    x = len(await ctx.channel.purge(limit=int(number)))
+    await ctx.respond(f'❌ Pomyślnie usunięto {x} wiadomości!', delete_after = .5)
 
 #clock command.... needs upgrade, (thumbnail is correct)
 @bot.command()
@@ -88,28 +107,17 @@ async def clock(ctx):
 @bot.command()
 async def logout(ctx):
     if ctx.author.id != 650343691998855188:
-        await ctx.respond("Nie masz wystarczających uprawnień :<", delete_after=1)
+        await ctx.respond("⛔ Nie masz wystarczających uprawnień :<", delete_after=.5)
     if ctx.author.id == 650343691998855188:
         msg = discord.Embed(title='Disconnecting.....', color=0x723535)
-        msg.remove_author
+        msg.remove_author()
         secs = abs(runStarted - time.perf_counter())
         runtiming = time.strftime("%H godzin %M minut %S sekund", time.gmtime(secs))
-        msg.add_field(name='------------------', value=f'{ctx.author.mention}, wylogowałeś mnie :< \n Byłem włączony przez : ``{runtiming}``\nMój ping wynosi : ``{round(bot.latency * 1000)}`` \n D1sScoo0nneCt1ing....', inline=True)
+        msg.add_field(name='------------------', value=f'{ctx.author.mention}, wylogowałeś mnie :< \n⏰ Byłem włączony przez : ``{runtiming}``\n⏰ Mój ping wynosi : ``{round(bot.latency * 1000)}`` \n D1sScoo0nneCt1ing....', inline=True)
         await ctx.respond(embed=msg)
         print("\n\n\n\nDISCONNECTING\n\n\n\n")
         await discord.Client.close(bot)
 
 #Moderation
-#
-#
-#
-#
-@bot.command()
-async def ban(ctx, 
-                user: Option(User, 'Użytkownik do zbanowania'),
-                reason: Option(str, 'Powód bana')):
-    await user.ban()
-    await ctx.respond(f'Użytkownik {user}, zostałx zbanowanx')
-
 
 bot.run(token)
