7d95abc73e4a6c2e71a98d8ead5dc39028dbf0c2
mattBright <mateuszwronski.yt@gmail.com>
2022-01-07 04:55:45 +0100

    :/

--- try.py
+++ try.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 
 bot = discord.Bot()
 
+txt = open('token.txt', "r")
+token = txt.read()
+txt.close()
 
 @bot.slash_command()
 async def hello(ctx, name: str = None):
@@ -13,4 +16,4 @@ async def hello(ctx, name: str = None):
 async def hi(ctx, user):
     await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
 
-bot.run("OTI3ODQxMzMwNzgzMjAzNDE5.YdQFkw.2bfoyg-q--JR1EFjp3ps3GyjVXo")
+bot.run(token)
