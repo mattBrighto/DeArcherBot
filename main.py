760b135f587f83fa5079f4d5089f76b40f40d5f7
mattBright <mateuszwronski.yt@gmail.com>
2022-01-07 13:57:54 +0100

    Alpha 1.0

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from typing_extensions import runtime
-import discord, time
+import discord, time, logging
 from discord.ext import commands
+
+#logging
+logging.basicConfig(level=logging.INFO)
+
+#useful variables
 runStarted = time.perf_counter()
 bot = discord.Bot()
 UserMatt = bot.get_or_fetch_user(650343691998855188)
 meMention = '<@650343691998855188>'
+
+#getting token
 file = open('Z:/token.txt', "r")
 token = file.read()
 file.close()
 
+#when the bot comes alive
 @bot.event
 async def on_ready():
     global meUser
     meUser = await bot.get_or_fetch_user(650343691998855188)
     print("The prefix is /")
     print('Bot is currently: Online')
-    print(type(meUser))
 
+#ping embeded command
 @bot.command()
 async def ping(ctx):
     msg = discord.Embed(title="PING Request", color=0xE06666)
@@ -26,6 +34,7 @@ async def ping(ctx):
     msg.add_field(name='------------', value=f'Jestem online, a mój ping wynosi ``{round(bot.latency * 1000)} ms``\nJestem włączony od : ``{runtiming}``', inline=False)
     await ctx.respond(embed=msg)
 
+#info embeded command
 @bot.command()  
 async def info(ctx):
     msg = discord.Embed(color=0x9C1ABC)
@@ -37,6 +46,7 @@ async def info(ctx):
     msg.add_field(name='Linki', value='``PERSONAL SITE``   https://mc.polishwrona.pl/', inline=False)
     await ctx.respond(embed=msg)
 
+#contact limited command
 @bot.command()
 async def contact(ctx, msg):
     list = []
@@ -46,23 +56,24 @@ async def contact(ctx, msg):
         global meUser
         dm = await meUser.create_dm()
         await dm.send(f'New message from {ctx.author.mention}: '+msg)
-        list.append(ctx.author.id)
+        list.append(int(ctx.author.id))
         await ctx.respond(f"||{ctx.author.mention}|| Twoja wiadomość została wysłana")
 
-
+#say owner command
 @bot.command()
 async def say(ctx, msg):
+    if ctx.author.id != 650343691998855188:
+        await ctx.respond("Nie masz wystarczających uprawnień :<", delete_after = .5)
     if ctx.author.id == 650343691998855188:
-        #await ctx.respond(".", delete_after=0.1)
         await ctx.send(msg)
-    else:
-        await ctx.respond("Nie masz wystarczających uprawnień :<", delete_after = 1)
-
+        
+#clear messages command.... but still want to display deleted messages not how many were supposed to be
 @bot.command()
 async def clear(ctx, x):
     await ctx.channel.purge(limit=int(x))
     await ctx.respond(f'Pomyślnie usunięto {x} wiadomości!', delete_after = 1)
 
+#clock command.... needs upgrade, (thumbnail is correct)
 @bot.command()
 async def clock(ctx):
     msg = discord.Embed(title="Zegar", color=0xFFFFFF)
@@ -71,8 +82,11 @@ async def clock(ctx):
     msg.add_field(name=time.strftime("%H:%M"), value="-------", inline=False)
     await ctx.respond(embed=msg)
 
+#logout owner command
 @bot.command()
 async def logout(ctx):
+    if ctx.author.id != 650343691998855188:
+        ctx.respond("Nie masz wystarczających uprawnień :<", delet_after=1)
     if ctx.author.id == 650343691998855188:
         msg = discord.Embed(title='Disconnecting.....', color=0x723535)
         msg.remove_author
@@ -82,7 +96,5 @@ async def logout(ctx):
         await ctx.respond(embed=msg)
         print("\n\n\n\nDISCONNECTING\n\n\n\n")
         await discord.Client.close(bot)
-    else:
-        ctx.respond("Nie masz wystarczających uprawnień :<", delet_after=1)
 
 bot.run(token)
